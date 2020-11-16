ab10535145f6da57b5b4a9fa9218d4156382c18d
Mars <wilson.marissarose@gmail.com>
2020-11-16 00:21:02 -0500

    Created pkg.json

--- employee-tracker.js
+++ employee-tracker.js
@@ -4,13 +4,10 @@ const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
     host: "localhost",
-
     // Your port; if not 3306
     port: 3306,
-
     // Your username
     user: "root",
-
     // Your password
     password: "allblack",
     database: "Employee_TrackerDB"
--- employee-tracker.sql
+++ employee-tracker.sql
@@ -4,20 +4,20 @@ CREATE database Employee_TrackerDB;
 USE Employee_TrackerDB;
 
 CREATE TABLE role (
-    roleID INT AUTO_INCREMENT NOT NULL,
+    roleID INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(10,2),
     departmentID INT(10)
 );
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(30),
     departmentID INT(10)
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     firstName VARCHAR(30) NOT NULL,
     lastName VARCHAR(30) NOT NULL,
     roleID INT(10),
